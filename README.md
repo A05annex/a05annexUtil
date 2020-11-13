6be63f68e2e975f596ef2b08603e186b76d0a65f
STHobbes <royster.hall@gmail.com>
2020-11-12 23:42:19 -0800

    First checkin of real code for this project, lots of work to do.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+* **version:** 0.8.0
+* **status:** in development - do not use this yet
+* **comments:** We are in the process of pulling this library out of a larger code base for release. Documentation and
+  testing are incomplete and/or inaccurate. We have not yet incorporated this library in our other software projects.
+
 # a05annexUtil
-Utility classes that are not dependent on First FTC/FRC infrastructure.
+Utility classes for use in First FTC/FRC robot code, but that are not dependent on either
+First FTC or First FRC infrastructure. We built this library as we realized that there 
+is a lot of common code (functionality) that we use both inside and outside the
+robot-specific code - for example, in autonomous path planning. We wanted to package that code so that it could be
+used regardless of the FTC/FRC base platform libraries and/or hardware.
+
+## a05annexUtil Library Contents
---
+++ src/main/java/org/a05annex/util/Utl.java
@@ -0,0 +1,85 @@
+package org.a05annex.util;
+
+/**
+ * This is a utility class with some commonly used math constants and utility methods
+ */
+public final class Utl {
+
+    /**
+     * Set to the value of {@link java.lang.Math#PI}, or {@code 180}&deg;, for convenience only - we are
+     * already including all of the constants from this class.
+     */
+    public static final double PI = Math.PI;
+    /**
+     * Set to the value of {@code -}{@link java.lang.Math#PI}, or {@code 180}&deg;, because we use this a
+     * lot in swerve drive code.
+     */
+    public static final double NEG_PI = -Math.PI;
+    /**
+     * Set to the value of {@code (}{@link java.lang.Math#PI}{@code  * 2,0)}, or {@code 360}&deg;, because we use
+     * this a lot in swerve drive code.
+     */
+    public static final double TWO_PI = Math.PI * 2.0;
+    /**
+     * Set to the value of {@code (}{@link java.lang.Math#PI}{@code  * 0.5)}, or {@code 90}&deg;, because we use
+     * this a lot in swerve drive code.
+     */
+    public static final double PI_OVER_2 = Math.PI * 0.5;
+    /**
+     * Set to the value of {@code -(}{@link java.lang.Math#PI}{@code  * 0.5)}, or {@code -90}&deg;, because
+     * we use this a lot in swerve drive code.
+     */
+    public static final double NEG_PI_OVER_2 = -(Math.PI * 0.5);
+
+    /**
+     * This class is all static constants and methods, it cannot be instantiated.
+     */
+    private Utl() {}
+
+    /**
+     * Get the length of an n-dimensional set of lengths.
+     *
+     * @param values The dimensional lengths. The number of dimensional lengths is variable and may be zero.
+     * @return The n-dimensional length, 0.0 of no dimensional lengths are specified.
+     */
+    public static double length(double ... values) {
+        double lengthSquared = 0.0;
+        for (double v : values) {
+            lengthSquared += v * v;
+        }
+        return Math.sqrt(lengthSquared);
+    }
+
+    /**
+     * Get the maximum value of an arbitrary number of values.
+     *
+     * @param values The values. The number of values is variable and may be zero.
+     * @return The maximum value, {@link Double#NEGATIVE_INFINITY} if no values are specified.
+     */
+    public static double max(double ... values) {
+        double max = Double.NEGATIVE_INFINITY;
+        for (double v : values) {
+            if (v > max) {
+                max = v;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Get the minimum value of an arbitrary number of values.
+     *
+     * @param values The values. The number of values is variable and may be zero.
+     * @return The minimum value, {@link Double#POSITIVE_INFINITY} if no values are specified.
+     */
+    public static double min(double ... values) {
+        double min = Double.POSITIVE_INFINITY;
+        for (double v : values) {
+            if (v < min) {
+                min = v;
+            }
+        }
+        return min;
+    }
+
+}
